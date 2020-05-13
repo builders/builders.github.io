b57e48ccf3327d7c513cd7e1f6f7960df2c9e30b
Stephen Bau <bauhouse@gmail.com>
2020-05-12 23:22:18 -0700

    Configure Eleventy templates

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,21 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.setTemplateFormats([
-    "css",
-    "md",
-    "svg"
-  ]);
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      layouts: "_includes/layouts"
+    },
+    passthroughFileCopy: true,
+    templateFormats: [
+      "css",
+      "html",
+      "ico",
+      "jpg",
+      "md",
+      "njk",
+      "png",
+      "svg"
+    ]
+  }
 };
