230fba919dd974dadffa85844239e0b2ce232c95
Stephen Bau <bauhouse@gmail.com>
2020-05-21 11:16:32 -0700

    Add data files for multiple sites

---
+++ src/_data/design.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const ghostContentAPI = require("@tryghost/content-api");
+
+// Init Ghost API
+const api = new ghostContentAPI({
+  url: process.env.DESIGN_API_URL,
+  key: process.env.DESIGN_API_KEY,
+  version: "v3"
+});
+
+// Get all site information
+module.exports = async function() {
+  const siteData = await api.settings
+    .browse({
+      include: "icon,url"
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
+  return siteData;
+};
