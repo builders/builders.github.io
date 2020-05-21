f2a47cf6ffcef341b390292e20de35615601a6e2
Stephen Bau <bauhouse@gmail.com>
2020-05-21 11:15:50 -0700

    Configure Ghost API for multiple sites

--- .eleventy.js
+++ .eleventy.js
@@ -4,13 +4,87 @@ const fs = require("fs");
 const ghostContentAPI = require("@tryghost/content-api");
 
 // Init Ghost API
-const api = new ghostContentAPI({
-    url: process.env.GHOST_API_URL,
-    key: process.env.GHOST_CONTENT_API_KEY,
+const imagine_api = new ghostContentAPI({
+    url: process.env.IMAGINE_API_URL,
+    key: process.env.IMAGINE_API_KEY,
+    version: "v3"
+});
+
+const design_api = new ghostContentAPI({
+    url: process.env.DESIGN_API_URL,
+    key: process.env.DESIGN_API_KEY,
+    version: "v3"
+});
+
+const build_api = new ghostContentAPI({
+    url: process.env.BUILD_API_URL,
+    key: process.env.BUILD_API_KEY,
+    version: "v3"
+});
+
+const socialarc_api = new ghostContentAPI({
+    url: process.env.SOCIALARC_API_URL,
+    key: process.env.SOCIALARC_API_KEY,
     version: "v3"
 });
 
 module.exports = function(config) {
+
+  // Get all imagine posts
+  config.addCollection("imagine_posts", async function(collection) {
+      collection = await imagine_api.posts
+          .browse({
+              include: "tags,authors",
+              limit: "all"
+          })
+          .catch(err => {
+              console.error(err);
+          });
+
+      return collection;
+  });
+
+  // Get all design posts
+  config.addCollection("design_posts", async function(collection) {
+      collection = await design_api.posts
+          .browse({
+              include: "tags,authors",
+              limit: "all"
+          })
+          .catch(err => {
+              console.error(err);
+          });
+
+      return collection;
+  });
+
+  // Get all build posts
+  config.addCollection("build_posts", async function(collection) {
+      collection = await build_api.posts
+          .browse({
+              include: "tags,authors",
+              limit: "all"
+          })
+          .catch(err => {
+              console.error(err);
+          });
+
+      return collection;
+  });
+
+  // Get all social architecture posts
+  config.addCollection("socialarc_posts", async function(collection) {
+      collection = await socialarc_api.posts
+          .browse({
+              include: "tags,authors",
+              limit: "all"
+          })
+          .catch(err => {
+              console.error(err);
+          });
+
+      return collection;
+  });
 
   config.setBrowserSyncConfig({
     callbacks: {
